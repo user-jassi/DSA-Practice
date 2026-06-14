995309c34cb3399acf14f619624cc36c9e723545
jassi <contact.singh.jaswant@gmail.com>
2026-06-15 00:08:01 +0530

    Commit against - Square Pattern | O(n2)

---
+++ Patterns/Pattern01_Square.java
@@ -0,0 +1,37 @@
+/**
+ *Problem: Print a Square Pattern of star for given number n
+ *Approach: Run the outer loop for n times , 
+            then Run the inner loop for n times to print * and 
+            give new line after completion of innner loop  
+  *Time Complexicity
+  *Space Complexicity
+
+*/
+
+
+
+public class Pattern01_Square{
+
+
+    public void printPattern(int num){
+
+        for(int i = 1; i<= num; i++ ){
+
+            for(int j =1; j<= num; j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        Pattern01_Square obj = new Pattern01_Square();
+
+        obj.printPattern(5);
+
+    }
+
+
+}
