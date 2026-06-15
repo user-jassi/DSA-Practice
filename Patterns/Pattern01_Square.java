7f89b94a1979ebaecafd8fd731c006deb0a6307a
jassi <contact.singh.jaswant@gmail.com>
2026-06-15 21:35:05 +0530

    commit against Pattern03|(n2)

--- Patterns/Pattern01_Square.java
+++ Patterns/Pattern01_Square.java
@@ -1,22 +1,14 @@
 /**
- *Problem: Print a Square Pattern of star for given number n
+ * Problem: Print a Square Pattern of star for given number n
  *Approach: Run the outer loop for n times , 
             then Run the inner loop for n times to print * and 
-            give new line after completion of innner loop  
-  *Time Complexicity
-  *Space Complexicity
-
+            give new line after completion of innner loop
+  *Time Complexity: O(n2)
+  * Space Complexity: O(1)
 */
-
-
-
 public class Pattern01_Square{
-
-
     public void printPattern(int num){
-
         for(int i = 1; i<= num; i++ ){
-
             for(int j =1; j<= num; j++){
                 System.out.print("* ");
             }
@@ -26,11 +18,8 @@ public class Pattern01_Square{
     }
 
     public static void main(String[] args){
-
         Pattern01_Square obj = new Pattern01_Square();
-
         obj.printPattern(5);
-
     }
 
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DSA-Practice
+Structured DSA preparation following Striver's A2Z roadmap.
+Solving problems daily and pushing solutions with approach and complexity notes.
