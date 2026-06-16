3f4ca8ac7acb03fcce9363684d44e379a83ecba1
jassi <contact.singh.jaswant@gmail.com>
2026-06-16 20:57:39 +0530

    Commit against Pattern05|(On2)

---
+++ Patterns/Pattern05.java
@@ -0,0 +1,34 @@
+/*
+*Problem: for n = 4
+       ****
+       ***
+       **
+       *
+
+* Approach: run the outer loop for n times , start iteration variable(i) from n and
+            make it minus by 1 fro each iteration.
+            inner loop will run for i times and print the *
+            
+
+*Time Complexity: O(n2)
+*Space Complexity: O(1)
+ */
+
+public class Pattern05 {
+
+    public void printPattern(int n){
+        for(int i =4; i>=1;i-- ){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public static void main(String[] args){
+
+        Pattern05 obj = new Pattern05();
+        obj.printPattern(4);
+
+    }
+}
