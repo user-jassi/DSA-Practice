942667c7c58906889da166c45a48f8f72f1eb93d
jassi <contact.singh.jaswant@gmail.com>
2026-06-18 21:35:06 +0530

    commit against Pattern09 with approach

--- Patterns/Pattern09.java
+++ Patterns/Pattern09.java
@@ -1,6 +1,25 @@
+
+/*Problem: for n = 4
+   *
+  ***
+ *****
+*******
+*******
+ *****
+  ***
+   *
+
+* Approach:  look for the symetry in the pattern, we can devided the pattern in two part, 
+             First half and second half then we can approach each half by figuring out the relation between numbers
+             of * or spaces and row number and n     
+*Time Complexity: O(n2)
+*Space Complexity: O(1)
+ */
+
 public class Pattern09 {
 
     public void printPattern(int n){
+        //First half
             for(int i =1; i<=n; i++){
                 for(int j =1; j<=(n-i); j++){
                     System.out.print(" ");
@@ -14,6 +33,8 @@ public class Pattern09 {
                 System.out.println("");
 
             }
+
+            //Second Half
 
             for(int i=n; i>=1;i-- ){
                 for(int j =1; j<=(n-i); j++){
